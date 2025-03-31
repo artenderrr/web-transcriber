7e12edccaa6abf5bc5624ef00c515c7116fa3f17
artender <artem1347929@gmail.com>
2025-03-31 20:29:05 +0300

    🐛 Fix RestartButton responsiveness issues

--- frontend/src/components/RestartButton.vue
+++ frontend/src/components/RestartButton.vue
@@ -10,7 +10,7 @@ function onClick() {
 
 <template>
   <div class="container" @click="onClick">
-    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 12 12"><path fill="currentColor" d="M10.5 2A.75.75 0 0 0 9 2v.646a4.5 4.5 0 1 0 1.42 4.206c.088-.465-.304-.852-.777-.852c-.355 0-.636.291-.711.638a3.001 3.001 0 1 1-1.266-3.133H7.25a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 .75-.75z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 12 12"><path fill="currentColor" d="M10.5 2A.75.75 0 0 0 9 2v.646a4.5 4.5 0 1 0 1.42 4.206c.088-.465-.304-.852-.777-.852c-.355 0-.636.291-.711.638a3.001 3.001 0 1 1-1.266-3.133H7.25a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 .75-.75z"/></svg>
   </div>
 </template>
 
@@ -34,5 +34,10 @@ function onClick() {
 
 .container > svg {
   display: block;
+}
+
+svg {
+  width: 3rem;
+  height: 3rem;
 }
 </style>
