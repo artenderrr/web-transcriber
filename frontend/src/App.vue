629aa258a7e6005569c4a8e7eefade7dad7e2e84
artender <artem1347929@gmail.com>
2025-03-31 19:28:31 +0300

    ✨ Add smooth transition between views

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
