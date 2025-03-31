89a278bfd9914e2bde5f94cd563f5077db0d9056
artender <artem1347929@gmail.com>
2025-03-31 19:05:04 +0300

    ✨ Add RestartButton component

---
+++ frontend/src/components/RestartButton.vue
@@ -0,0 +1,38 @@
+<script setup>
+import router from "../router";
+import store from "../store";
+
+function onClick() {
+  URL.revokeObjectURL(store.audioUrl);
+  router.push("/");
+}
+</script>
+
+<template>
+  <div class="container" @click="onClick">
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 12 12"><path fill="currentColor" d="M10.5 2A.75.75 0 0 0 9 2v.646a4.5 4.5 0 1 0 1.42 4.206c.088-.465-.304-.852-.777-.852c-.355 0-.636.291-.711.638a3.001 3.001 0 1 1-1.266-3.133H7.25a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 .75-.75z"/></svg>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background-color: #2b2b2b;
+  color: #5b5b5b;
+
+  width: 5rem;
+  aspect-ratio: 1 / 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: .125rem solid #3b3b3b;
+  border-radius: 50%;
+
+  cursor: pointer;
+}
+
+.container > svg {
+  display: block;
+}
+</style>
--- frontend/src/views/PlayerView.vue
+++ frontend/src/views/PlayerView.vue
@@ -1,9 +1,18 @@
 <script setup>
+import RestartButton from "../components/RestartButton.vue";
 import store from "../store";
 </script>
 
 <template>
   <div class="wrapper">
+    <RestartButton />
     <audio v-if="store.audioUrl" :src="store.audioUrl" controls></audio>
   </div>
 </template>
+
+<style scoped>
+.wrapper {
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
