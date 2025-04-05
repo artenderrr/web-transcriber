fada28c754a5bfdf7b602d7c9ef447290e3e5cbf
artender <artem1347929@gmail.com>
2025-04-05 08:53:24 +0300

    ✨ Add endpoint for checking transcription status

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, UploadFile, HTTPException
+from celery.result import AsyncResult
+from app.worker import worker
 from app.services.transcription import TranscriptionService
 
 app = FastAPI()
@@ -9,3 +11,12 @@ async def request_transcription(audio: UploadFile) -> dict[str, str]:
         raise HTTPException(status_code=400, detail="Filename is missing")
     task_id = await TranscriptionService.request_transcription(audio)
     return {"task_id": task_id}
+
+@app.get("/transcriptions/{task_id}/state")
+def check_transcription_state(task_id: str) -> dict[str, str]:
+    task: AsyncResult[None] = worker.AsyncResult(task_id)
+    try:
+        state = task.state
+    except AttributeError:
+        raise HTTPException(status_code=404, detail="Task with provided ID doesn't exist")
+    return {"state": state}
