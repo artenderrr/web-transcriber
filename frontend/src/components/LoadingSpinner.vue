e139f9bb82aa2dbbcf88429ee6cb379365b5e1f2
artender <artem1347929@gmail.com>
2025-04-07 13:58:02 +0300

    ✨ Add layout for loading spinner on transcription request

---
+++ frontend/src/components/LoadingSpinner.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="loading-spinner" viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="400" cy="400" fill="none"
+      r="300" stroke-width="115" stroke="#5b5b5b"
+      stroke-dasharray="1350 1400"
+      stroke-linecap="round" />
+  </svg>
+</template>
+
+<style scoped>
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading-spinner {
+  animation: spin 2s linear infinite;
+}
+</style>
