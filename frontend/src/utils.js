78136487248e7b979933bba55a7525f13d63eb74
artender <artem1347929@gmail.com>
2025-04-08 19:25:18 +0300

    ✨ Add polling in TranscriptionView

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,7 +1,8 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export async function fetchState(taskId) {
   let state;
   try {
-    const apiUrl = import.meta.env.VITE_API_URL;
     const response = await fetch(`${apiUrl}/transcriptions/${taskId}/state`);
     const data = await response.json();
     state = data["state"];
@@ -10,4 +11,17 @@ export async function fetchState(taskId) {
     state = "FAILURE";
   }
   return state;
+}
+
+export async function fetchResult(taskId) {
+  let blob, text;
+  try {
+    const response = await fetch(`${apiUrl}/transcriptions/${taskId}`);
+    blob = await response.blob();
+    text = await blob.text();
+  } catch (error) {
+    console.error(error);
+    [blob, text] = [null, null];
+  }
+  return [blob, text];
 }
--- frontend/src/views/TranscriptionView.vue
+++ frontend/src/views/TranscriptionView.vue
@@ -1,5 +1,26 @@
 <script setup>
+import { ref, onMounted } from "vue";
+import { fetchState, fetchResult } from "../utils";
 import LoadingSpinner from "../components/LoadingSpinner.vue";
+import ErrorModal from "../components/ErrorModal.vue";
+import router from "../router";
+import store from "../store";
+
+const state = ref(null);
+
+async function poll() {
+  state.value = await fetchState(store.taskId);
+  if (state.value === "SUCCESS") {
+    const [blob, text] = await fetchResult(store.taskId);
+    store.transcriptionBlob = blob;
+    store.transcriptionText = text;
+    router.push("/result");
+  } else if (state.value === "STARTED") {
+    setTimeout(poll, 500);
+  }
+}
+
+onMounted(poll);
 </script>
 
 <template>
@@ -9,10 +30,25 @@ import LoadingSpinner from "../components/LoadingSpinner.vue";
       <span class="upper-text">Переводим в текст.</span>
       <span>Пожалуйста, подождите...</span>
     </div>
+    <Transition name="fade">
+      <ErrorModal v-if="state === 'FAILURE'"
+      header-content="Ой..."
+      main-content="Что-то пошло не так. Пожалуйста, попробуйте ещё раз."
+      button-content="Ладно"
+      :button-action="() => router.go(-2)" />
+    </Transition>
   </div>
 </template>
 
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+
 .wrapper {
   flex-direction: column;
   gap: 1.25rem;
