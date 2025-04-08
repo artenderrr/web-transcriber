3b6940f812dfacd77957337f1b26a09f34ecd64a
artender <artem1347929@gmail.com>
2025-04-08 16:06:26 +0300

    ✨ Add demo TranscriptionView

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,11 +3,13 @@ import { createRouter, createMemoryHistory } from "vue-router";
 import RecordingView from "./views/RecordingView.vue";
 import PlayerView from "./views/PlayerView.vue";
 import PendingView from "./views/PendingView.vue";
+import TranscriptionView from "./views/TranscriptionView.vue";
 
 const routes = [
   { path: "/", component: RecordingView },
   { path: "/player", component: PlayerView },
-  { path: "/pending", component: PendingView }
+  { path: "/pending", component: PendingView },
+  { path: "/transcription", component: TranscriptionView }
 ];
 
 const router = createRouter({
---
+++ frontend/src/views/TranscriptionView.vue
@@ -0,0 +1,39 @@
+<script setup>
+import LoadingSpinner from "../components/LoadingSpinner.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <LoadingSpinner class="loading-spinner" />
+    <div class="transcription-text-container">
+      <span class="upper-text">Переводим в текст.</span>
+      <span>Пожалуйста, подождите...</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  flex-direction: column;
+  gap: 1.25rem;
+
+  color: #5b5b5b;
+}
+
+.loading-spinner {
+  width: 4.5rem;
+}
+
+.transcription-text-container {
+  font-size: 1.75rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.upper-text {
+  color: #424242;
+}
+</style>
