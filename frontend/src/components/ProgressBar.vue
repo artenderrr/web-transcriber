603f3573f9a58125ca7a4e9bea6dcaad1033138c
artender <artem1347929@gmail.com>
2025-04-09 17:10:22 +0300

    ✨ Add ProgressBar

---
+++ frontend/src/components/ProgressBar.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(["progress"]);
+const progress = computed(() => props.progress || 0);
+</script>
+
+<template>
+  <div class="progress-bar-container">
+    <div class="progress-bar-value" :style="{ width: progress + '%' }"></div>
+  </div>
+</template>
+
+<style scoped>
+.progress-bar-container {
+  background-color: #2b2b2b;
+
+  width: 20rem;
+  height: 1rem;
+
+  border-radius: .75rem;
+
+  overflow: hidden;
+}
+
+.progress-bar-value {
+  background-color: #5b5b5b;
+
+  height: 100%;
+
+  transition: width .125s;
+}
+</style>
